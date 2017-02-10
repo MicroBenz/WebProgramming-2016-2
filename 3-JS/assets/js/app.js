d1661c3f0a871bdcdd5b58094071c849cc3eecb0
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-02-10 20:19:10 +0700

    Homework 3 (JS): Completed!

--- 3-JS/assets/js/app.js
+++ 3-JS/assets/js/app.js
@@ -7,19 +7,47 @@ var STAMP_CROWN = 'crown';
 
 var selectedStamp = '';
 
+// Helper Function
+function getStampImageSource() {
+    return 'assets/img/stamp_mark_' + selectedStamp + '.png';
+}
+
+function createStampElement() {
+    return $('<img src="' + getStampImageSource() +'" class="mark-proto">');
+}
+
+function createCursorUrl() {
+    return 'url(' + getStampImageSource() + ')';
+}
+
+// Main Logic for stamper
 $('.stamper').click(function() {
+    if (selectedStamp !== '') {
+        $('.stamper[stamper=' + selectedStamp + ']').next().removeClass('active');
+    }
+    $(this).next().addClass('active');  
     selectedStamp = $(this).attr('stamper');
 });
 
 $('#stamppad-canvas').click(function(e) {
     if (selectedStamp === '') return;
+    // Get mouse position
     var xPos = e.pageX - $(this).offset().left;
     var yPos = e.pageY - $(this).offset().top;
-    console.log(xPos, yPos);
-    var stampImageSrc = 'assets/img/stamp_mark_' + selectedStamp + '.png';
-    var stampElem = $('<img src="' + stampImageSrc +'" class="mark-proto">');
-    stampElem.css('position', 'absolute');
-    stampElem.css('left', xPos - stampElem[0].width / 2);        
-    stampElem.css('top', yPos - stampElem[0].height / 2);
+    // Create image DOM element
+    var stampElem = createStampElement();
+    stampElem.css({
+        position: 'absolute',
+        left: xPos - stampElem[0].width / 2,
+        top: yPos - stampElem[0].height / 2
+    });
     $(this).append(stampElem);
 });
+
+$('#stamppad-canvas').mousemove(function() {
+    if (selectedStamp === '') return;
+    var tempElem = createStampElement();
+    var stampWidth = tempElem[0].width;
+    var stampHeight = tempElem[0].height;
+    $('#stamppad-canvas').css('cursor', createCursorUrl() + ' ' + stampWidth / 2 + ' ' + stampHeight / 2 + ', auto');
+})
